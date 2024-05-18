8fbe6f040f3628ba90bcf0634d6dafe31598bfa2
James Wallace <sausages@gmail.com>
2024-05-18 15:07:36 -0400

    feat: various
    add a start/end shortcut, as well as shift arrow shortcuts
    colons now show at the start/end of the bars
    output columns are lined up

--- keys.php
+++ keys.php
@@ -2,4 +2,10 @@
 <?php
 include 'include/config.inc';
 
-$k = new Keys();
+if (file_exists("dump.out")) {
+    echo "restoring dump.out\n";
+    $k = unserialize(file_get_contents("dump.out"));
+    $k->gameLoop();
+} else {
+    $k = new Keys();
+}
